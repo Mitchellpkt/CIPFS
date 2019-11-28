16c6163c7320bb98f0b8ebf2a1cf6c5a9c3ca5e1
MitchellPKT <MitchellPKT@protonmail.com>
2019-11-27 16:53:18 -0800

    Uploading side works

--- CIPFS_upload.sh
+++ CIPFS_upload.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 
-echo CIPFS uploader
+# Say hi
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo "CIPFS uploader (by Isthmus)"
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# bash generate random 32 character alphanumeric string (upper and lowercase) 
+# Generate  random 32 character alphanumeric string (upper and lowercase) 
 # Code from https://gist.github.com/earthgecko/3089509
-NEW_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+KEY=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+echo ... generated key
 
-echo symmetric $1 $1.pgp
+# Encrypt file
+gpg --yes --batch --passphrase=$KEY -c $1
+echo ... encrypted file
 
-gpg --yes --batch --passphrase=$NEW_UUID -c $1
+# Note... here's how to decrypt:
+# gpg --yes --batch --passphrase=[Enter your passphrase here] filename.txt.gpg
 
-echo key = $NEW_UUID
+# Upload to IPFS
+IPFS_OUTPUT=`ipfs add $1.gpg`
+echo ... uploaded to IPFS
+
+IPFS_HASH=$(echo $IPFS_OUTPUT | awk '{print $2}') # Can I silence this?
+
+echo ~~~~~~~~~~~~~~
+echo CIPFS_address:
+echo $IPFS_HASH$KEY
+
