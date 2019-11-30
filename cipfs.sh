8e3deb2c9ce7c72bf9a998ba73624cf7ca40cdfc
MitchellPKT <MitchellPKT@protonmail.com>
2019-11-30 12:06:21 -0800

    Cleaned up the formatting

--- cipfs.sh
+++ cipfs.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Say hi
 echo
 echo \*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*
 echo "    Confidential IPFS, v0.1"
@@ -12,10 +11,10 @@ echo NOTICES:
 echo -  CIPFS requires standard PGP and IPFS to be installed
 echo -  This implementation provides OFF-DEVICE security
 echo -  Feel free to improve, modify, and distribute CIPFS code \& protocol
-echo  \ \ \(please include attribution and PR useful tweaks to origin repo\)
+echo \ \ \(please include attribution and PR useful tweaks to origin repo\)
 echo -  Default behavior is deterministic key generation
-echo  \ \ \i.e. symmetric encrypt with first 32 chars of SHA-256 sum, so that:
-echo   \ \ identical files ==\> identical ciphertexts ==\> no IPFS bloat
+echo \ \ \i.e. symmetric encrypt with first 32 chars of SHA-256 sum, so that:
+echo \ \ identical files ==\> identical ciphertexts ==\> no IPFS bloat
 echo   -  If your threat model is sensitive to IPFS storage hypothesis testing, 
 echo   \ \ "then add --random-key flag to generate a unique one-time key"
 echo 
@@ -23,41 +22,46 @@ echo --------
 echo PROGRESS:
 echo
 
-#########
-## upload functionality
+############
+############
+## CIPFS GET
+############
+############
+
 if [ "$1" == "add" ]; then
 
-# Random or deterministic key?
-if [[ " $* " == *' --random-key '* ]]
-then
-# Generate  random 32 character alphanumeric string (upper and lowercase) 
-# Code from https://gist.github.com/earthgecko/3089509
+####################
+## What type of key?
+
+# If --random-key, then generate random 32-char alphanumeric key
+if [[ " $* " == *' --random-key '* ]]; then
 KEY=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 echo ... generated random key
+
+# If not `--random-key` then use first 32 chars of sha256sum as key
 else
 FHASH=$(sha256sum $2)
-echo hash is:
-echo $FHASH
 KEY=$(echo "$FHASH" | cut -b 1-32)
-echo key is
-echo $KEY
 echo ... generated deterministic key
 fi
 
-# Encrypt file
+
+####################
+## Encrypt the file
 gpg --yes --batch --passphrase=$KEY --cipher-algo AES256 -c $2
 echo ... encrypted $2
 	
-# Upload to IPFS
+####################
+## Upload to IPFS
 echo ... uploading to IPFS
 echo
 IPFS_OUTPUT=`ipfs add $2.gpg`
 echo
 rm $2.gpg
 
-# Extract IPFS name
+####################
+## Extract IPFS key
 IPFS_HASH=$(echo $IPFS_OUTPUT | awk '{print $2}')
-
 echo
 echo \*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*
 echo Retrieve file with this CIPFS ticket:
@@ -67,31 +71,36 @@ echo
 
 fi
 
-	
-#########
-## download
+############
+############
+## CIPFS GET
+############
+############
+
 if [ "$1" == "get" ]; then
 
-# Split the name ($1) into hash & key
-# First 46 characters are IPFS address
-# Remaining 32 characters are the symmetric decryption key
-x=$2 # er4rur843tru43f8fyu7weyf7wyf7whfygewvuw6fft6wftewgfwegf6ewgfcweygwluhvyrewvgykr
+####################
+## Extract the address & key
+x=$2
 FNAME=$(echo "$x" | cut -b 2-47)
 KEY=$(echo "$x" | cut -b 48-$((48+32)))
 
-# Download from to IPFS
+####################
+## Download from IPFS
 IPFS_OUTPUT=`ipfs get $FNAME`
 echo
 echo ... downloaded from IPFS
 mv $FNAME $FNAME.gpg
 
-# Decrypt the file 
+####################
+## Decrypt the file
 gpg --yes --batch --quiet --cipher-algo AES256 --passphrase=$KEY $FNAME.gpg
 rm $FNAME.gpg
 mv $FNAME C$FNAME
-	
 echo ... decrypted file
 
+####################
+## Display file info
 echo
 echo \*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*
 echo Retrieved file name:
